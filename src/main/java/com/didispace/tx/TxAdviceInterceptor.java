5e6791fd313f6e9a6d363f3c2c51e7b2d96440c0
txc <1273586908@qq.com>
2017-12-09 17:04:43 +0800

    申明式事物配置：TxAdviceInterceptor

--- src/main/java/com/didispace/tx/TxAdviceInterceptor.java
+++ src/main/java/com/didispace/tx/TxAdviceInterceptor.java
@@ -1,9 +1,10 @@
 package com.didispace.tx;
 
-import java.util.List;
-import java.util.Properties;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-//import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
@@ -12,73 +13,57 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
 import org.springframework.transaction.interceptor.RollbackRuleAttribute;
 import org.springframework.transaction.interceptor.RuleBasedTransactionAttribute;
+import org.springframework.transaction.interceptor.TransactionAttribute;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
 
-import com.google.common.collect.Lists;
-
-//@Aspect
-//@Configuration
+/**
+ * Created by txc on 2017/8/28.
+ */
+@Aspect
+//@Component 事务依然生效
+@Configuration
 public class TxAdviceInterceptor {
-    /**
-     * 需要事务管理的包路径
-     */
-    private static final String AOP_POINTCUT_EXPRESSION = "execution(* com.service.*Impl.*(..))";
+
+    private static final int TX_METHOD_TIMEOUT = 10;
+    private static final String AOP_POINTCUT_EXPRESSION = "execution (* com.didispace.web.*.*(..))";
 
     @Autowired
     private PlatformTransactionManager transactionManager;
 
-
-    /*事务拦截类型*/
-    public TransactionInterceptor defaultTransactionInterceptor(PlatformTransactionManager transactionManager,
-                                                                List<Class<? extends Exception>> additionalRollbackRuleExceptions) {
-        TransactionInterceptor transactionInterceptor = new TransactionInterceptor();
-        Properties transactionAttributes = new Properties();
-
-        List<RollbackRuleAttribute> rollbackRules = Lists.newArrayList();
-        rollbackRules.add(new RollbackRuleAttribute(Exception.class));
-        // TODO voir si on ajoute SecurityServiceException.class en fonction de ce que ça donne sur le Wombat
-        // ou voir si on ne la dégage pas carrément en fait...
-        //回滚异常
-        if(additionalRollbackRuleExceptions != null && !additionalRollbackRuleExceptions.isEmpty()){
-            for (Class<? extends Exception> clazz : additionalRollbackRuleExceptions) {
-                rollbackRules.add(new RollbackRuleAttribute(clazz));
-            }
-        }
-        DefaultTransactionAttribute readOnlyTransactionAttributes =
-                new DefaultTransactionAttribute(TransactionDefinition.PROPAGATION_REQUIRED);
-        readOnlyTransactionAttributes.setReadOnly(true);
-
-        RuleBasedTransactionAttribute writeTransactionAttributes =
-                new RuleBasedTransactionAttribute(TransactionDefinition.PROPAGATION_REQUIRED, rollbackRules);
-
-        String readOnlyTransactionAttributesDefinition = readOnlyTransactionAttributes.toString();
-        String writeTransactionAttributesDefinition = writeTransactionAttributes.toString();
-        // read-only
-        transactionAttributes.setProperty("is*", readOnlyTransactionAttributesDefinition);
-        transactionAttributes.setProperty("has*", readOnlyTransactionAttributesDefinition);
-        transactionAttributes.setProperty("get*", readOnlyTransactionAttributesDefinition);
-        transactionAttributes.setProperty("list*", readOnlyTransactionAttributesDefinition);
-        transactionAttributes.setProperty("search*", readOnlyTransactionAttributesDefinition);
-        transactionAttributes.setProperty("find*", readOnlyTransactionAttributesDefinition);
-        transactionAttributes.setProperty("count*", readOnlyTransactionAttributesDefinition);
-        // write et rollback-rule
-        transactionAttributes.setProperty("*", writeTransactionAttributesDefinition);
-
-        transactionInterceptor.setTransactionAttributes(transactionAttributes);
-        transactionInterceptor.setTransactionManager(this.transactionManager);
-        return transactionInterceptor;
+    @Bean
+    public TransactionInterceptor txAdvice() {
+        NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+         /*只读事务，不做更新操作*/
+        RuleBasedTransactionAttribute readOnlyTx = new RuleBasedTransactionAttribute();
+        readOnlyTx.setReadOnly(true);
+        readOnlyTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_NOT_SUPPORTED );
+        /*当前存在事务就使用当前事务，当前不存在事务就创建一个新的事务*/
+        RuleBasedTransactionAttribute requiredTx = new RuleBasedTransactionAttribute();
+        requiredTx.setRollbackRules(
+                Collections.singletonList(new RollbackRuleAttribute(Exception.class)));
+        requiredTx.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        requiredTx.setTimeout(TX_METHOD_TIMEOUT);
+        Map<String, TransactionAttribute> txMap = new HashMap<>();
+        txMap.put("add*", requiredTx);
+        txMap.put("save*", requiredTx);
+        txMap.put("insert*", requiredTx);
+        txMap.put("update*", requiredTx);
+        txMap.put("delete*", requiredTx);
+        txMap.put("get*", readOnlyTx);
+        txMap.put("query*", readOnlyTx);
+        source.setNameMap( txMap );
+        TransactionInterceptor txAdvice = new TransactionInterceptor(transactionManager, source);
+        return txAdvice;
     }
 
-    /**
-     * 切面拦截规则
-     */
     @Bean
     public Advisor txAdviceAdvisor() {
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
         pointcut.setExpression(AOP_POINTCUT_EXPRESSION);
-        return new DefaultPointcutAdvisor(pointcut, this.defaultTransactionInterceptor(this.transactionManager,null));
+        return new DefaultPointcutAdvisor(pointcut, txAdvice());
+        //return new DefaultPointcutAdvisor(pointcut, txAdvice);
     }
 }
