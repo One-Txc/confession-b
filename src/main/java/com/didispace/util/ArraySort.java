4d67b2a84ef921e084193d202dd687740ad51718
txc <1273586908@qq.com>
2018-12-28 15:34:55 +0800

    abc

---
+++ src/main/java/com/didispace/util/ArraySort.java
@@ -0,0 +1,31 @@
+package com.didispace.util;
+
+/**
+ * @author: txc
+ * @date: 18-11-15 下午5:19
+ */
+public class ArraySort implements Runnable {
+
+    private int number;
+
+    public ArraySort(int number) {
+        this.number = number;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{339,102, 338,338,339, 62, 9132, 580, 666,101};
+        for (int number : numbers) {
+            new Thread(new ArraySort(number)).start();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(this.number);
+            System.out.println(this.number);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/resources/static/database.sql
+++ src/main/resources/static/database.sql
@@ -6,12 +6,15 @@ CREATE TABLE `main_config` (
   `right_button_text` varchar(4) DEFAULT NULL,
   `tel` varchar(256) DEFAULT NULL COMMENT '手机号',
   `taobao_account` varchar(256) DEFAULT NULL COMMENT '淘宝账号',
+  `state` varchar(2) DEFAULT NULL COMMENT '状态[0-不可用,1-可用]',
   `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '生效时间',
   `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '结束时间',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`main_config_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8mb4 COMMENT='配置表';
+
+todo 加个状态
 
 
 CREATE TABLE `popup_config` (
@@ -28,6 +31,30 @@ CREATE TABLE `popup_config` (
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`popup_config_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COMMENT='弹窗配置表';
+
+
+CREATE TABLE `certificate` (
+  `certificate_code` varchar(64) NOT NULL COMMENT '凭证code',
+
+
+
+  `popup_config_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增Id',
+  `main_config_id` int(10) unsigned NOT NULL COMMENT '主配置id',
+  `group_type` varchar(256) DEFAULT NULL COMMENT '分组[ok-ok按钮的配置,no按钮的配置]',
+  `order_index` int(2) unsigned NOT NULL COMMENT '排序,越小越考前',
+  `type` varchar(256) DEFAULT NULL COMMENT 'type(msg-消息提示,html-弹框)',
+  `icon` int(10) unsigned DEFAULT NULL COMMENT '图标',
+  `title` varchar(256) DEFAULT NULL COMMENT '标题',
+  `path` varchar(256) DEFAULT NULL COMMENT '路径',
+  `content` varchar(256) DEFAULT NULL COMMENT '内容',
+
+
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`popup_config_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb4 COMMENT='添加配置凭证';
+
+
 
 
 create user 'confession'@'%' identified by '123456';
